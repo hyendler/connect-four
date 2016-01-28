797f83261f5d88aba837b910a8d9f5b1c2ba5a8e
Hanah Yendler & Lindsey Stevenson <pair+hyendler.lindsey-s@devbootcamp.com>
2016-01-28 15:33:18 -0800

    fuaslkdhg;akdghs

--- js/models.js
+++ js/models.js
@@ -13,8 +13,6 @@ var board = {
 function Game(board, user) {
   this.board = board;
   this.user = user;
-  };
-
   //check if game is won?
     //return true or false
       //continue playing if false
@@ -22,44 +20,69 @@ function Game(board, user) {
 };
 
 //place piece function which takes the column on which the user picked and it needs to update the board and return the correct cell and color
-Game.prototype.placePiece = function(columnNumber) {
-  var result = []; // [correctCell, pieceColor]
-  var correctCell = // this is the logic - maybe result of helper method
-  var pieceColor = this.user.getColor(); // push to result at index 1
-  // now with result built - update board - another helper method
-  return result;
-};
+
 
 Game.prototype.updateBoard = function(columnNumber) {
   var pieceColor = this.user.getColor();
   var cellToUpdate = "";
-  var cellHash = this.board.find(columnNumber);
+  var cellHash = this.board[columnNumber];
 
-  for (var key in this.board) {
-    if (key === columnNumber) {
-      var searchColumn = this.board[key];
-      // this should return a hash like this one:
-      // {1: "black", 2: "red", 3: null, 4: null, 5: null, 6: null}
-      for (var key in searchColumn) {
-        if (searchColumn[key] === null) {
-          cellToUpdate = "cell" + key;
-          searchColumn[key] = pieceColor;
-          break;
-        };
-      };
-    };
-    break;
+  for (var key in cellHash) {
+    if (cellHash[key] === null) {
+      cellToUpdate = "cell" + key;
+      cellHash[key] = pieceColor;
+      break
+    }
   };
-  return cellToUpdate;
-}
+
+  //   if (key == columnNumber) {
+  //     console.log("I am here");
+  //     var searchColumn = this.board[key];
+  //     console.log(searchColumn);
+  //     console.log("--");
+  //     // this should return a hash like this one:
+  //     // {1: "black", 2: "red", 3: null, 4: null, 5: null, 6: null}
+  //     for (var key in searchColumn) {
+  //       if (searchColumn[key] === null) {
+  //         this.cellToUpdate = "cell" + key;
+  //         console.log(cellToUpdate);
+  //         console.log("**");
+  //         searchColumn[key] = pieceColor;
+  //         break;
+  //       };
+  //     };
+  //   };
+  //   break;
+  // };
+  return cellToUpdate; // i.e. "cell2"
+};
+
+Game.prototype.placePiece = function(columnNumber) {
+  var result = []; // [correctCell, pieceColor]
+  var pieceColor = this.user.getColor();
+  var correctCell = this.updateBoard(columnNumber);
+  result.push(correctCell, pieceColor);
+  return result;
+};
 
 
 // helper methods (will try to move to another file later)
 
 //user object
 function User(color) {
-  var color = color;
-  var getColor = function() {
+  this.getColor = function() {
     return color;
   }
 }
+
+
+//Drive Code
+var user1 = new User("black");
+var user2 = new User("red");
+var game = new Game(board, user1);
+var result = game.placePiece("col4");
+console.log(result);
+
+
+
+
